6545776b91e5ba8ba09dc0a02ea4d11edb629403
zhaofeng-shu33 <616545598@qq.com>
2021-04-25 18:19:13 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L https://desktop.githubusercontent.com/releases/2.7.2-5b94c877/GitHubDesktopSetup.exe -o /home/curl_user/a.zip
+curl -L "https://ia903009.us.archive.org/zip_dir.php?path=/17/items/DragonBall1986.zip&formats=MATROSKA,ARCHIVE%20BITTORRENT,METADATA,ITEM%20TILE" -o /home/curl_user/a.zip
